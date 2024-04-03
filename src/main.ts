a5c5a95483582d8af57ded1769012559372a73e1
WH934426 <wh934426@outlook.com>
2024-04-03 22:42:33 +0800

    feat: 清除默认模板代码

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import 'normalize.css/normalize.css'
 import '@/styles/index.scss'
 import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
