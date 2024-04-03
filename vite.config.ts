2442428c0831fe3f1161bf84437aab1fc07b68a7
WH934426 <wh934426@outlook.com>
2024-04-03 22:24:57 +0800

    feat: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +12,16 @@ export default defineConfig({
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
-      imports: ["vue","vue-router","pinia"]
+      imports: ['vue', 'vue-router', 'pinia']
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+    extensions: ['.vue', '.ts', '.js', '.json']
+  }
 })
