49cc4c901afcc6ca0320fa258df112eea532cfed
WH934426 <wh934426@outlook.com>
2024-04-03 22:38:24 +0800

    feat: 清除默认样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'normalize.css/normalize.css'
 import '@/styles/index.scss'
 import 'element-plus/dist/index.css'
 
