52e0133cfb30029c5df2386b242236e630bc153b
WH934426 <wh934426@outlook.com>
2024-04-03 22:37:12 +0800

    feat: 添加scss公共样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import '@/styles/index.scss'
 import 'element-plus/dist/index.css'
 
 createApp(App).mount('#app')
